677a12fbaf63caa7a1cd312a853aed337d8162c5
iwishdanwouldinsideme <johnokbuddy5@gmail.com>
2025-05-13 18:00:02 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,54 +1,79 @@
-// Events can be added in any order.
-
 const timelineData = [
   {
-    title: "H3 and Hasan First Met",
-    date: "2021-08-01",
-    description: "Ethan Klein from H3 Podcast and political streamer Hasan Piker meet for the first time and announce a new podcast collaboration called 'The Leftovers'.",
-    source: "https://www.youtube.com/watch?v=Gq7WwuGPmJw",
-    sourceTitle: "H3 Podcast Announcement"
+    title: "Hasan Piker first appears on H3 Podcast #207",
+    date: "2020-08-17",
+    description: `Hasan Piker appeared on H3 Podcast #207, where he discussed politics and his rise on Twitch.
+    <iframe src="https://www.youtube.com/embed/TpimNx4PJoY"></iframe>`,
+    sources: [
+      { url: "https://www.youtube.com/watch?v=TpimNx4PJoY", title: "Hasan Piker - H3 Podcast #207" }
+    ]
   },
+  
   {
-    title: "Beginning of The Leftovers Podcast",
-    date: "2021-09-05", 
-    description: "H3 and Hasan launch their new political podcast 'The Leftovers' to discuss current events and politics from a left-leaning perspective.",
-    source: "https://www.youtube.com/watch?v=jcK3H2h_8Hk",
-    sourceTitle: "First Leftovers Episode"
+    title: "Launch of the \"Leftovers\" political podcast with Hasan and Ethan",
+    date: "2021-09-26",
+    description: `Hasan and Ethan launched their podcast 'Leftovers' on September 26, 2021.
+    <iframe src="https://www.youtube.com/embed/uASzNUFT5Dk"></iframe>`,
+    sources: [
+      { url: "https://www.youtube.com/watch?v=uASzNUFT5Dk", title: "And The New Host Is... Leftovers #1" }
+    ]
   },
+
   {
-    title: "Initial Tensions Surface",
-    date: "2023-02-15",
-    description: "First public signs of disagreement between H3 and Hasan over content direction and political takes.",
-    source: "https://twitter.com/h3h3productions/status/1625982341",
-    sourceTitle: "Twitter Discussion"
+    title: "Ethan quietly unfollowed Frogan on Twitter.",
+    date: "2023-10-11",
+    description: `<p>Following her October 7th post:</p>
+    <blockquote class="twitter-tweet"><p lang="en" dir="ltr">leftists preach and foam at the mouth at the thought of a revolution happening in america, but as soon as it happens in the middle east what they’re doing is wrong</p>&mdash; frogan (@fr0gan) <a href="https://twitter.com/fr0gan/status/1710587819385614829?ref_src=twsrc%5Etfw">October 7, 2023</a></blockquote>
+    <p> Ethan quietly unfollowed Frogan on Twitter, which led to her complaining over it on her Discord, accusing Ethan of being a zionist racist that supports the IDF.</p>
+    <img src="https://preview.redd.it/ethan-klein-unfollowed-frogan-v0-f9fb9766lltb1.jpg?width=1080&crop=smart&auto=webp&s=592b67af1ae1bc33de091c81391808c4571a63e4"/>
+    As well as posting about it on her Twitter:
+    <blockquote class="twitter-tweet"><p lang="en" dir="ltr">tfw one of your favorite content creators unfollows for being pro palestine LMAO</p>&mdash; frogan (@fr0gan) <a href="https://twitter.com/fr0gan/status/1712060807758905494?ref_src=twsrc%5Etfw">October 11, 2023</a></blockquote>`,
+    sources: [
+      { url: "https://twitter.com/fr0gan/status/1710587819385614829", title: "Frogan Tweet – Oct 7, 2023" },
+      { url: "https://twitter.com/fr0gan/status/1712060807758905494", title: "Frogan Tweet – Oct 11, 2023" }
+    ]
   },
+
   {
-    title: "Major Public Fallout",
-    date: "2023-05-10",
-    description: "H3 and Hasan have a public disagreement on stream that leads to increased tension between their communities.",
-    source: "https://www.youtube.com/watch?v=example123",
-    sourceTitle: "Stream Clip"
+    title: "Ethan & Hasan debate Oct 7 attacks.",
+    date: "2023-10-12",
+    description: `In episode #61 of Leftovers, Ethan and Hasan debated the October 7 Hamas attacks and the Israel–Gaza war.
+    <iframe src="https://www.youtube.com/embed/JFznOHunD_c"></iframe>`,
+    sources: [
+      { url: "https://www.youtube.com/watch?v=JFznOHunD_c", title: "Israel vs Gaza - Leftovers #61" }
+    ]
   },
+
   {
-    title: "End of The Leftovers Podcast",
-    date: "2023-06-01",
-    description: "H3 and Hasan announce the end of their collaborative podcast amid growing creative differences.",
-    source: "https://twitter.com/hasanthehun/status/1664729384",
-    sourceTitle: "Official Announcement"
+    title: "Ethan announces Leftovers will likely be on pause.",
+    date: "2023-11-28",
+    description: `Ethan announces Leftovers will likely be on pause going forward. He says it's something he will be discussing with Hasan.
+    <iframe src="https://www.youtube.com/embed/vGqJWGZTpcI?start=11612"></iframe>`,
+    sources: [
+      { url: "https://www.youtube.com/live/vGqJWGZTpcI&t=11612", title: "Hasan Piker – H3 Podcast Wiki (03:13:32)" }
+    ]
   },
+
   {
-    title: "Community Drama Escalates",
-    date: "2023-07-15",
-    description: "Fan communities of both creators begin taking sides, with Reddit and Twitter becoming battlegrounds for arguments.",
-    sourceType: "image",
-    source: "https://example.com/drama-screenshot.jpg",
-    sourceTitle: "Social Media Screenshots"
+    title: "Ethan confirmed Leftovers is over.",
+    date: "2023-11-29",
+    description: `Ethan confirmed Leftovers is over on SYNT #53.
+    <video controls width="640">
+      <source src="https://packaged-media.redd.it/zw9ljbsrb63c1/pb/m2-res_360p.mp4?m=DASHPlaylist.mpd&v=1&e=1747162800&s=f4b9ac85c07b3877d3b306bdcfe65e6fd25dc856" type="video/mp4">
+      Your browser does not support the video tag.
+    </video>`,
+    sources: [
+      { url: "https://www.youtube.com/watch?v=ddeLw46n6lU", title: "Dan's Tech Tips - See You Next Tuesday #53" }
+    ]
   },
+
   {
-    title: "Recent Developments",
-    date: "2023-11-20",
-    description: "Latest updates in the ongoing situation between H3, Hasan, and their respective communities.",
-    source: "https://www.twitch.tv/hasanabi/clip/example456",
-    sourceTitle: "Recent Statement"
+    title: "Frogan tweets 'this mf never shuts up' in reference to Ethan.",
+    date: "2024-09-12",
+    description: `frogan posted a tweet saying, "this mf never shuts up oh my god", in response to an Ethan's Instagram Story.
+    <blockquote class="twitter-tweet"><p lang="en" dir="ltr">this mf never shuts up oh my god <a href="https://t.co/TBFYHdI1q5">https://t.co/TBFYHdI1q5</a></p>&mdash; frogan (@fr0gan) <a href="https://twitter.com/fr0gan/status/1834306362446414310?ref_src=twsrc%5Etfw">September 12, 2024</a></blockquote>`,
+    sources: [
+      { url: "https://twitter.com/fr0gan/status/1834306362446414310", title: "Frogan Tweet – Sep 12, 2024" }
+    ]
   }
-]
+];
